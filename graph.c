28d08e9f091c79bc8388a77c48d430f7ec5fb7be
Your Name <you@example.com>
2026-05-14 20:41:04 +0000

    new try

--- graph.c
+++ graph.c
@@ -39,7 +39,7 @@ void addNode(Graph* g, const char* label)
     if (map_search(g->adjacencyMap, (void*) label) != NULL) return;
 
     char* nuevo = malloc(sizeof(char));
-    nuevo = strcpy(label);
+    nuevo = strcpy(label, nuevo);
     
     List* lista = list_create();
 
