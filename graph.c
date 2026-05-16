688d312fe9e62a0a381deed3cc764f8be0f23c9e
Your Name <you@example.com>
2026-05-16 22:11:25 +0000

    new try

--- graph.c
+++ graph.c
@@ -48,48 +48,24 @@ void addNode(Graph* g, const char* label)
     map_insert(g->adjacencyMap, nuevo, lista);
     
     return;
-}MapPair* pair = map_search(g->adjacencyMap, (void*) src);
-if (pair == NULL) return;
-if (map_search(g->adjacencyMap, (void*) dest) == NULL) return;
-List* lista = pair->value;
-
-Edge* edge = (Edge*) malloc(sizeof(Edge));
-
-edge->target = strdup(dest);
-edge->weight = weight;
-
-list_pushBack(lista, edge);
+}
 
 void addEdge(Graph* g, const char* src, const char* dest, int weight) 
 {
     if (!g || !src || !dest) return;
+
+    MapPair* pair = map_search(g->adjacencyMap, (void*) src);
     
+    if (pair == NULL) return;
+    if (map_search(g->adjacencyMap, (void*) dest) == NULL) return;
+    List* lista = pair->value;
 
-        MapPair* pair = map_search(g->adjacencyMap, (void*)src);
+    Edge* edge = (Edge*) malloc(sizeof(Edge));
 
-        if (pair == NULL)return;
+    edge->target = strdup(dest);
+    edge->weight = weight;
 
-        if (map_search(g->adjacencyMap, (void*)dest) == NULL)return;
-
-        List* lista = (List*)pair->value;
-
-        Edge* edge = (Edge*)malloc(sizeof(Edge));
-
-        if (edge == NULL) return;
-
-        edge->target = (char*)malloc(strlen(dest) + 1);
-
-        if (edge->target == NULL) {
-            free(edge);
-            return;
-        }
-
-        strcpy(edge->target, dest);
-
-        edge->weight = weight;
-
-        list_pushBack(lista, edge);
-    
+    list_pushBack(lista, edge);
     
     return;
 
@@ -127,7 +103,7 @@ int getWeight(Graph* g, const char* label1, const char* label2)
         {
             return aux->weight;
         }
-        list_next(lista);
+        aux = list_next(lista);
     }
     
 
